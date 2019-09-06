c6650cfdabb6e72684b299c30d24f1c7315bba61
felixtsu <humaeks@gmail.com>
2019-09-06 10:31:50 +0800

    updates show numbers implementation, registering frameHandler

--- main.ts
+++ main.ts
@@ -5,10 +5,24 @@
 //% groups='["Actions"]'
 namespace cubicbird {
 
+    let infos: { [key: string]: any } = {}
+
     //%block
-    export function showNumber(no: number) {
-        screen.fillRect(0, 120 - (image.font8.charHeight + 2), image.font8.charWidth + 2 + 10, image.font8.charHeight + 2, 3)
-        screen.fillRect(0, 120 - (image.font8.charHeight + 1), image.font8.charWidth + 1 + 10, image.font8.charHeight + 1, 10)
-        screen.print(no.toString(), 0, 110)
+    export function showNumber(tag: string, value: any) {
+        infos[tag] = value
     }
+
+    function drawInfo() {
+        infos.forEach((value: any, key: string) => {
+            screen.fillRect(0, 120 - (image.font8.charHeight + 2), image.font8.charWidth + 2 + 10, image.font8.charHeight + 2, 3)
+            screen.fillRect(0, 120 - (image.font8.charHeight + 1), image.font8.charWidth + 1 + 10, image.font8.charHeight + 1, 10)
+            screen.print(key + ":" + value.toString(), 0, 110)
+        })
+    }
+
+
+    game.eventContext().registerFrameHandler(scene.HUD_PRIORITY, function () {
+        drawInfo()
+    })
+
 }
